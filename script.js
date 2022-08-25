c90404b56131e1f9471cdef6808016a0669abeb0
Serginho915 <serginho915@gmail.com>
2022-08-25 16:11:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,35 @@
-let arr  = [];
+let sports = [
+    ['skier','⛷'],
+    ['snowboarder','🏂'],
+    ['apple','🍎'],
+    ['hockey','🏒'],
+    ['ice skate','⛸'],
+    ['swimmer','🏊'],
+    ['surfer','🏄‍'],
+    ['watermelon','🍉'],
+    ['lemon','🍋'],
+    ['rowboat','🚣'],
+    ['bicyclist','🚴‍']
+];
 
-let lengthArr = +prompt('Enter length of array');
+let summer_sports = sports.slice(5);
 
-if(lengthArr){
-    while(lengthArr < 2 || lengthArr > 10){
-        lengthArr = prompt('Enter length of array');
-    }
-    
-    if(lengthArr < 0 ) lengthArr = Math.abs(lengthArr);
-    if(!(lengthArr % 1 === 0))  lengthArr = Math.round(lengthArr);
+let winter_sports = sports.slice(0, 5);
 
-    arr = [lengthArr];
-}
-let minElem = +prompt('Enter min value');
-while(minElem > 10 || minElem < -10){
-    minElem = +prompt('Enter min value');
-}
-if(!minElem % 1 === 0) minElem = Math.round(minElem);
+let fruits = summer_sports.splice(2,2);
+fruits.push(winter_sports.splice(2,1)[0]);
 
-let maxElem = +prompt('Enter max value');
-while(maxElem > 50 || maxElem < minElem){
-    maxElem = +prompt('Enter max value');
-}
-if(!maxElem % 1 === 0) maxElem = Math.round(maxElem);
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(minElem);
-    max = Math.floor(maxElem);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(`*** Winter sports ***`);
+for(let i=0; i<winter_sports.length; i++){
+   console.log(winter_sports[i].join(`: `));
 }
 
-for(let i=1; i<=lengthArr - 1; i++){
-        arr[arr.length] = getRandomIntInclusive(minElem, maxElem);
-    }
-
-let minEl = maxEl = arr[0];
-
-for(let i=1; i<arr.length; i++){
-    if(arr[i] < minEl){
-        minEl = arr[i];
-    }
-
-    if(arr[i] > maxEl){
-        maxEl = arr[i];
-    }
+console.log(`*** Summer sports ***`);
+for(let i=0; i<summer_sports.length; i++){
+   console.log(summer_sports[i].join(`: `));
 }
 
-let indexOfMin = arr.indexOf(minEl);
-let indexOfMax = arr.indexOf(maxEl);
-let tempVal = arr[indexOfMin]; 
-arr[indexOfMin] = arr[indexOfMax];
-arr[indexOfMax] = tempVal;
-
-console.log(arr);
-console.log(minEl, maxEl);
+console.log(`*** Fruits ***`);
+for(let i=0; i<fruits.length; i++){
+   console.log(fruits[i].join(`: `));
+}
