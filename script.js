f9792556004ae8decc9ca2230897568a778fdf93
Serhii Kozhyn <serginho915@gmail.com>
2022-09-14 19:11:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-const products = [
-	['apple',10],
-	['banana',8],
-	['mango',20],
-	['grape',18]
-]
-function summerValue(value){
-	return value*0.8;
-}
+const obj = {
+    x: 10,
+    y: 20,
+	h: 40,
+    inner: {
+        x: 20,
+        z: 30
+    },
+    foo2: {
+        k: 23,
+        p: 13
+    }
+} 
 
-function winterValue(value){
-	return value*2;
-}
 
-function sumOfProductsFunc(arr){
-	let sum = 0;
-	for (let i = 0; i < arr.length ; i++) {		
-		sum += arr[i][1];	
-	} 
-	return sum;
-}
-function getPrice(products, summerValue){
-	let copiedPrice = [...products];
-	let sumOfProducts;
-		if(typeof summerValue === `function`){
-	        for (let i = 0; i < copiedPrice.length; i++) {
-				for (let j = 0; j < copiedPrice[i].length; j++) {
-					if(typeof copiedPrice[i][j] === `number`)copiedPrice[i][j] = summerValue(copiedPrice[i][j]);
-				}
-			} 
-			return copiedPrice;
-	    } else{
-			sumOfProducts = sumOfProductsFunc(copiedPrice);
+function convert(obj){
+
+	let objCopy ={};
 	
-			return sumOfProducts;
-	    }
+	for (let key in obj) {
+		if (!(typeof (obj[key]) == "object")){
+			
+		objCopy[key] = obj[key];
+			
+		}
+		else{
+			Object.assign(objCopy, obj[key])
+			convert(obj[key])
+		}
+		
+	}
 
+	return objCopy;
 }
-console.log( getPrice(products));
+
+
+console.log(convert(obj)) ;
