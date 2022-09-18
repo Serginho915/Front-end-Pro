84800338811a7ff7eca85d52303c60192fba4cf0
Serhii Kozhyn <serginho915@gmail.com>
2022-09-18 20:20:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
-const obj = {
-    x: 10,
-    y: 20,
-    inner: {
-        x: 20,
-        z: 30
+const vegetables = [
+    {
+        name: `tomato`,
+        icon: `🍅`,
+        price: 2.3
     },
-    foo2: {
-        k: 23,
-        p: 13
+    {
+        name: `carrot`,
+        icon: `🥕`,
+        price: 1.5
+    },
+    {
+        name: `corn`,
+        icon: `🌽`,
+        price: 2.78,
+        season: true
+    },
+
+];
+
+const fruits = [
+    {
+        name: `watermelon`,
+        icon: `🍉`,
+        price: 7.7,
+        season: true
+    },
+    {
+        name: `cherries`,
+        icon: `🍒`,
+        price: 8.5,
+        season: true
+    },
+    {
+        name: `pineapple`,
+        icon: `🍍`,
+        price: 9.8
     }
-} 
 
+];
 
-function convert(obj){
-
-	let objCopy ={};
-	
-	for (let key in obj) {
-		if (!(typeof (obj[key]) == "object")){
-			
-		objCopy[key] = JSON.parse(JSON.stringify(obj[key]));
-			
-		}
-		else{
-			Object.assign(objCopy, obj[key])
-			convert(obj[key])
-		}
+let Product = {
+	getPrice(arr){
+		  arr.map(obj => {
+            debugger;
+            if(obj.season) return obj.price *= this.seasonKoef;
+            else  return obj.price;
+        });
 		
-	}
+	},
 
-	return objCopy;
+	getInfo(arr){
+        this.getPrice(arr);
+        arr.forEach(obj => {
+            return str =`Product ${obj.icon}${obj.name}.Type ${this.type}. Price ${obj.price}`;
+        });
+	}
+};
+
+makePrototype(arr, objectProto){
+
 }
 
 
-console.log(convert(obj)) ;
+
+const Vegetable = Object.create(Product);
+Vegetable.type = `Vegetable`;
+Vegetable.seasonKoef = 1.3;
+
+const Fruit = Object.create(Product);
+Vegetable.type = `Fruit`;
+Vegetable.seasonKoef = 2;
+
+
+
+
