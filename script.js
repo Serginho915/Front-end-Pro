df183736a02a6c5a1fa59a2a84de92e73c81d469
Serhii Kozhyn <serginho915@gmail.com>
2022-09-24 16:21:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,31 @@
-const vegetables = [
-    {
-        name: `tomato`,
-        icon: `🍅`,
-        price: 2.3
-    },
-    {
-        name: `carrot`,
-        icon: `🥕`,
-        price: 1.5
-    },
-    {
-        name: `corn`,
-        icon: `🌽`,
-        price: 2.78,
-        season: true
-    },
+class Bulka{
+    constructor(name, size, ingredients = [`cutlet`, `salada`, `tomato`]){
+        this.name = name;
+        this.size = size;
+        this.ingredients = ingredients;
 
-];
-
-const fruits = [
-    {
-        name: `watermelon`,
-        icon: `🍉`,
-        price: 7.7,
-        season: true
-    },
-    {
-        name: `cherries`,
-        icon: `🍒`,
-        price: 8.5,
-        season: true
-    },
-    {
-        name: `pineapple`,
-        icon: `🍍`,
-        price: 9.8
     }
 
-];
-debugger;
-let finalArray = [];
-let Product = {
-    getInfo(){
-        debugger;
-
-        return `Product${this.icon} ${this.name}.Type ${this.type}. Price ${this.getPrice()}`;
-    },
-    getPrice(){
-        debugger;
-        let priceKF = 0;
-        if(this.season) priceKF = this.price * this.seasonKoef;
-        else priceKF = this.price
-        return priceKF;
-    }
-}
-
-function makePrototype(arr, objectProto){
-    debugger;
-    arr = JSON.parse(JSON.stringify(arr));
-    return arr
-        .map(product => {
-            let newObj = Object.create(objectProto); 
-            Object.assign(newObj, product); 
-            return newObj;
+    setAdditionalIngredients(...arr){
+        arr.map( item =>{
+            this.ingredients.push(item);
         })
+       
+    }
 }
 
-function renderList(arr){
-    debugger;
-    let LIs = arr
-        .map(product => `<li>${product.getInfo()}</li>`)
-        .join(``);
-    return `<ul>${LIs}</ul>`;
+class Cheeseburger extends Bulka{
+    constructor(name, size, ingredients = [`cutlet`, `salada`, `tomato` , `cheese`]){
+        super(name, size, ingredients);
+    }
 }
 
 
-const Vegetable = Object.create(Product);
-Vegetable.type = `Vegetable`;
-Vegetable.seasonKoef = 1.3;
 
-const Fruit = Object.create(Product);
-Fruit.type = `Fruit`;
-Fruit.seasonKoef = 2;
+let Humburger = new Bulka(`Humburger`, `small`);
+Humburger.setAdditionalIngredients(`egg`, `onion`);
 
-document.write(renderList(makePrototype(fruits,Fruit)));
+
+let myCheeseburger = new Cheeseburger(`Cheeseburger`, `small`);
+myCheeseburger.setAdditionalIngredients(`egg`, `onion`);
+console.log(myCheeseburger);
