9b836670474112709d103675ae923310b372d09c
Serhii Kozhyn <serginho915@gmail.com>
2022-11-23 23:13:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
-class Bulka{
-    constructor(name, size, ingredients = [`cutlet`, `salada`, `tomato`]){
-        this.name = name;
-        this.size = size;
-        this.ingredients = ingredients;
 
-    }
+const userData = {
+	USD: 1000,
+	EUR: 900,
+	UAH: 15000,
+	BIF: 20000,
+	AOA: 100
+};
 
-    setAdditionalIngredients(...arr){
-        arr.map( item =>{
-            this.ingredients.push(item);
-        })
-       
+const bankData = {
+    USD: {
+        max: 3000,
+        min: 100,
+        img: '💵'
+    },
+    EUR: {
+        max: 1000,
+        min: 50,
+        img: '💶'
+    },
+    UAH: {
+        max: 0,
+        min: 0,
+        img: '💴'
+    },
+    GBP: {
+        max: 10000,
+        min: 100,
+        img: '💷'
     }
 }
 
-class Cheeseburger extends Bulka{
-    constructor(name, size, ingredients = [`cutlet`, `salada`, `tomato` , `cheese`]){
-        super(name, size, ingredients);
-    }
-}
+const getMoney =  new Promise(
+        function(resolve, reject) {
+        let userChoice = confirm(`Do you want check balance?`);
+         userChoice ? resolve() : reject();
+      });
 
 
 
-let Humburger = new Bulka(`Humburger`, `small`);
-Humburger.setAdditionalIngredients(`egg`, `onion`);
+getMoney
+    .then(
+        ()=>{
 
+            let curr;
+            do{
+                 curr = prompt(`enter currency`)
+              } 
+              while (!(curr in userData) );
+            console.log(`Your balance is ${userData[curr]} ${curr}`)
+        },
+        ()=>{
+            let cash;
+             
+            do{
+                cash = prompt(`choose currency`)
+            }
+            while (!(cash in bankData) && !(cash in userData) );
+            if(bankData[cash].max <= 0) {
+                console.log(`ATM hasnt this currency`)
 
-let myCheeseburger = new Cheeseburger(`Cheeseburger`, `small`);
-myCheeseburger.setAdditionalIngredients(`egg`, `onion`);
-console.log(myCheeseburger);
+            }
+            let amount = prompt(`enter amount`)
+
+            if(amount > bankData[cash].max || amount > userData[cash]) console.log(`The amount entered is greater than available. Max amount: ${userData[cash]} ${cash}`)
+            else if(amount < bankData[cash].min) console.log(`The amount entered is less than available. Min amount: ${bankData[cash].min}  ${cash}`)
+            else console.log(`Take yours ${amount} ${cash}`)
+
+        }
+
+)
+.finally(
+    console.log(`Have a nice day!`)
+)
